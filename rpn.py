401099260af9cd573e9df2de9e7d3790a5ad8fdd
Derek <yaandofficial@gmail.com>
2019-10-30 13:52:00 -0400

    End of lecture 8

--- rpn.py
+++ rpn.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-	pass
+    stack = list()
+    for token in arg.split():
+        if token == '+':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        elif token == '-':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+        
+    if len(stack) != 1:
+        raise TypeError('malformed input')
+
+    return stack.pop()    
 
 def main():
-	while True:
-		calculate(input("rpn calc> "))
+    while True:
+        calculate(input("rpn calc> "))
 
 if __name__ == '__main__':
-	main()
+    main()
